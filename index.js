a385c403e5a848856f42a564b4b879b39892c5e0
Marc <marc.diaz.7e8@itb.cat>
2025-05-15 10:44:54 +0200

    SUBIR

---
+++ index.js
@@ -0,0 +1,42 @@
+// Importamos las bibliotecas necesarias
+import express from "express";
+
+// Importamos las rutas
+
+/*
+import notificacionesRoute from "./routes/notificacions.js";
+import reservasRoute from "./routes/reservas.js";
+import recursosRoute from "./routes/recursos.js";
+import usuariosRoute from "./routes/usuarios.js";
+*/
+//Configuración
+import { PORT } from "./config.js";
+
+
+
+// Creamos la aplicación
+const app = express();
+app.use(express.json())
+
+
+//EJS
+app.use(express.static("public"));//carpeta publica pel css
+app.set('view engine','ejs');//Fem servir el motor ejs
+app.set('views', './views'); //carpeta on desem els arxius .ejs
+
+
+//Endpoints Auth
+app.get("/", (req, res)=> {
+    res.render("index")
+});
+/*
+/// Usamos las rutas de la carpetea routes
+app.use("/notificaciones", notificacionesRoute);
+app.use("/reservas", reservasRoute);
+app.use("/recursos",recursosRoute);
+app.use("/usuarios", usuariosRoute);
+*/
+// Iniciamos el servidor en un solo puerto
+app.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
